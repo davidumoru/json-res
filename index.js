e5de50dc728275162d49027ae11bd2471ddb384c
David Umoru <umorudavido@gmail.com>
2023-09-07 11:15:06 +0100

    Refactor code

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = 5003;
 
-app.get('/api', (req, res) => {
+app.get("/api", (req, res) => {
   // Get query parameters
   const slackName = req.query.slack_name;
   const track = req.query.track;
 
   // JSON response
   const jsonResponse = {
-    slackName: slackName,
-    currentDay: new Date().toLocaleDateString('en-US', { weekday: 'long' }),
-    utcTime: new Date().toISOString(),
+    slack_name: slackName,
+    current_day: new Date().toLocaleDateString("en-US", { weekday: "long" }),
+    utc_time: new Date().toISOString(),
     track: track,
-    githubFileUrl: `https://github.com/davidumoru/json-res/blob/main/index.js`,
-    githubRepoUrl: 'https://github.com/davidumoru/json-res',
-    statusCode: 200,
+    github_file_url: `https://github.com/davidumoru/json-res/blob/main/index.js`,
+    github_repo_url: "https://github.com/davidumoru/json-res",
+    status_code: 200,
   };
 
   res.json(jsonResponse);
